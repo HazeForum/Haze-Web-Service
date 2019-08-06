eda196419cdc84b580e96c98fe24893519845b0c
iJhefe <35247554+iJhefe@users.noreply.github.com>
2019-08-06 20:15:00 -0300

    Update ErrorLibrary.php

--- Application/Core/ErrorLibrary.php
+++ Application/Core/ErrorLibrary.php
@@ -7,7 +7,8 @@ namespace Core;
 class ErrorLibrary
 {
     const Error = [
-      'Ex00A' => 'Endpoint not found'
+        'Ex00A' =>  'Endpoint not found',
+        'Ex01A' =>  'Item not found'
     ];
 
     public static function out(String $Code)
