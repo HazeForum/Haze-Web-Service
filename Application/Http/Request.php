7d41792d6bcea1660a1103d844c79287925b5f5b
iJhefe <35247554+iJhefe@users.noreply.github.com>
2019-08-06 16:46:59 -0300

    Update Request.php
    Added post and get request.

--- Application/Http/Request.php
+++ Application/Http/Request.php
@@ -6,8 +6,85 @@ namespace Http;
 
 class Request
 {
-    public static function get($url, $params)
+    const CustomMethods = [
+      'PUT',
+      'DELETE'
+    ];
+
+    /**
+     * @param String $Url
+     * @param array $Params
+     * @return bool|string
+     */
+    public static function get(String $Url, Array $Params)
     {
+
+        $Url .= '?' . Helper::get_params_str($Params);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $Url,
+        ]);
+
+        $res = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $res;
+    }
+
+    /**
+     * @param String $Url
+     * @param array $Params
+     * @return bool|string
+     */
+    public static function post(String $Url, Array $Params)
+    {
+
+        $ParamsStr = Helper::get_params_str($Params);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $Url,
+            CURLOPT_POSTFIELDS => $ParamsStr
+        ]);
+
+        $res = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $res;
+    }
+
+    /**
+     * @param array $Params
+     * @param String $Method
+     * @return bool
+     */
+    public static function is_getted(Array $Params, String $Method = 'GET')
+    {
+
+        $Method = strtoupper($Method);
+
+        if ($_SERVER['REQUEST_METHOD'] <> $Method)
+            return false;
+
+        $Method = array_key_exists($Method, self::CustomMethods) ? $_REQUEST : ($Method == 'GET' ? $_GET : $_POST);
+
+        var_dump($_POST);
+
+        foreach ($Params as $param):
+
+            if ( !isset($Method[$param]) )
+                return false;
+
+        endforeach;
+
+        return true;
 
     }
 
