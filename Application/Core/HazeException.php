70efc6442999bc56a0e1a58bca13ae7d84cb5091
iJhefe <35247554+iJhefe@users.noreply.github.com>
2019-08-06 17:51:47 -0300

    Updated to use Error Library
    Now this will show an exception message with error code and error library url

--- Application/Core/HazeException.php
+++ Application/Core/HazeException.php
@@ -4,18 +4,29 @@
 namespace Core;
 use Exception;
 
+
 class HazeException extends Exception
 {
-    public function __construct($message, $code = 0, Exception $previous = null) {
+    private $ErrorMessage;
 
-        parent::__construct($message, $code, $previous);
+    public function __construct(String $code) {
+
+        $this->ErrorMessage = ErrorLibrary::out($code);
+
+        self::CustomException($code);
+        parent::__construct( $this->ErrorMessage );
+
     }
 
     public function __toString() {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}. For more details check our Error Library on 'https://hws.hazeforum.com'";
+        return __CLASS__ . ": {$this->ErrorMessage}. ";
     }
 
-    public function BrazilizanExcepetion() {
-        // TODO
+    public static function CustomException($code) {
+        echo "
+            <h2>An error has occurred. </h2>
+            <h4>Code: [$code]</h4>
+            <h5>For more details check our Error Library on https://hws.hazeforum.com</h5>
+        ";
     }
 }
