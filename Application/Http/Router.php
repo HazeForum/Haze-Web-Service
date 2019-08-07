1da98e64a3c45fcd7d0dfe2059bf7d904f839690
iJhefe <35247554+iJhefe@users.noreply.github.com>
2019-08-06 22:05:42 -0300

    Create Router.php

---
+++ Application/Http/Router.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Http;
+
+
+use Core\Api\PathFinder;
+use Core\Api\Response;
+
+class Router
+{
+    /**
+     * @param bool $Api_Router
+     */
+    public static function init(Bool $Api_Router = false)
+    {
+        if (!isset($_GET['r']))
+            return Response::success('Running');
+
+        $Routes = self::mount_route($_GET['r']);
+
+        ( $Api_Router ? self::use_api_router( $Routes[0], $Routes[1]) :  self::use_layout_router() );
+
+    }
+
+    /**
+     * @param String $Route
+     * @return array
+     */
+    public static function mount_route(String $Route)
+    {
+        return explode('/', $Route);
+    }
+
+    public static function use_layout_router(Array $Routes)
+    {
+
+    }
+
+    /**
+     * @param String $Endpoint
+     * @param String $Item
+     */
+    public static function use_api_router(String $Endpoint, String $Item)
+    {
+        PathFinder::get($Endpoint, $Item);
+    }
+}
