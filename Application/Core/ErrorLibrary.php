cfb94b7d1ba5a84accd53fa58f54d231c85db3e8
iJhefe <35247554+iJhefe@users.noreply.github.com>
2019-08-06 23:33:35 -0300

    Update ErrorLibrary.php

--- Application/Core/ErrorLibrary.php
+++ Application/Core/ErrorLibrary.php
@@ -8,7 +8,8 @@ class ErrorLibrary
 {
     const Error = [
         'Ex00A' =>  'Endpoint not found',
-        'Ex01A' =>  'Item not found'
+        'Ex01A' =>  'Item not found',
+        'Ex200' =>  'Invalid Request'
     ];
 
     public static function out(String $Code)
