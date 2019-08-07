02e04f9358faf5413066e07aa131fed1b72b1fcb
iJhefe <35247554+iJhefe@users.noreply.github.com>
2019-08-07 00:42:08 -0300

    add config-global

--- init.php
+++ init.php
@@ -3,6 +3,8 @@ DEFINE("DOCUMENT_ROOT", dirname(__FILE__) );
 DEFINE("DS", DIRECTORY_SEPARATOR);
 session_start();
 
+require 'config-global.php';
+
 require 'Vendor/Autoload.php';
 
 Autoload::register();
