510a51ca0f50e26cd297f873e68f4a16a3d4a4c1
iJhefe <35247554+iJhefe@users.noreply.github.com>
2019-08-07 00:41:03 -0300

    Create config-global.php

---
+++ config-global.php
@@ -0,0 +1,17 @@
+<?php
+
+DEFINE('CONFIG_GLOBAL', [
+
+    ## Connection Configuration
+
+    'Connection' => [
+
+        'host'      => '127.0.0.1',             # IP or URL to Connect | Use 0.0.0.0 for don't use Connection Module
+        'username'  => 'root',                  # Username
+        'password'  =>  '',                     # Password
+        'database'  =>  'webservice',           # Database Name
+        'port'      =>  3306                    # Default: 3306
+
+    ],
+
+]);
